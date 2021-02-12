ee0293486ae6e7dbbcb5d6f2195782005c5267d9
Zsolt Szakal <60626421+zsoltszakal@users.noreply.github.com>
2021-02-12 07:25:27 +0000

    Update number-guessing-game.py

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,1 +1,29 @@
-x
+from random import randrange
+
+print("Welcome to the Guessing Game")
+print("I am thinking of a number between 1 and 100.")
+
+level = input("\nChoose a difficulty. Type 'easy' or 'hard' :  \n\n")
+
+random_number = randrange(100)
+
+if level == "easy":
+	remaining_life = 10
+else:
+	remaining_life = 5
+
+while not remaining_life == 0:
+	print(f"\n\nYou have {remaining_life} attempts left to guess the number.")
+	guess = int(input("\nMake a guess: \n "))
+	if guess > random_number:
+		remaining_life = remaining_life - 1
+		print("\nIt is too high.")
+	elif guess < random_number:
+		remaining_life = remaining_life - 1
+		print("\nIt is too low. ")
+	else:
+		remaining_life = 0
+		print(f"\nWell done! You got it! The answer was {guess}")
+
+if remaining_life == 0 and guess != random_number:
+	print("\nYou ran out of attemps. You lost game over")
